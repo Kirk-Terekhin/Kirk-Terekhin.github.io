7ad600f909b58313ad3d015a7bb9ea572914c4b9
Kirill <kirk.terekhin@gmail.com>
2020-07-16 09:54:41 +0600

    New Portfolio

--- source/devel/_layout/_action_button/_action_button.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var hamburger = document.querySelector('.hamburger');
-    hamburger.addEventListener('click', function() {
-        document.body.classList.toggle('unity_active');
-    });
-});
